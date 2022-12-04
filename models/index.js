5abb63e9cd4de795b91e0fcbf6f1460fa1bc010a
rjschroeder <are.jay.schroeder@gmail.com>
2022-12-04 00:22:39 -0600

    wrote product-category association

--- models/index.js
+++ models/index.js
@@ -5,6 +5,10 @@ const Tag = require('./Tag');
 const ProductTag = require('./ProductTag');
 
 // Products belongsTo Category
+Product.belongsTo(Category, {
+  foreignKey: "product_id",
+  onDelete: "CASCADE"
+})
 
 // Categories have many Products
 
