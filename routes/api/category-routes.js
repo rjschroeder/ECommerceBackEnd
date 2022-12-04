5a0290644b19d2ab92ae6be1b4c76c91044c0996
rjschroeder <are.jay.schroeder@gmail.com>
2022-12-04 00:38:22 -0600

    added try catch to all routes

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -21,18 +21,38 @@ router.get('/', (req, res) => {
 router.get('/:id', (req, res) => {
   // find one category by its `id` value
   // be sure to include its associated Products
+  try {
+
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
 router.post('/', (req, res) => {
   // create a new category
+  try {
+
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
 router.put('/:id', (req, res) => {
   // update a category by its `id` value
+  try {
+
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
 router.delete('/:id', (req, res) => {
   // delete a category by its `id` value
+  try {
+
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
 module.exports = router;
