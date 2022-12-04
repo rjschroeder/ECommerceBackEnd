e208242cd3450cad01ea4b482e66ad8c064dafbd
rjschroeder <are.jay.schroeder@gmail.com>
2022-12-04 00:37:08 -0600

    route to find all categories

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -6,6 +6,16 @@ const { Category, Product } = require('../../models');
 router.get('/', (req, res) => {
   // find all categories
   // be sure to include its associated Products
+  try{
+    Category.findAll({
+      include: [Product]
+    })
+      .then((response) => {
+        res.status(200).json(response);
+      })
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
 router.get('/:id', (req, res) => {
